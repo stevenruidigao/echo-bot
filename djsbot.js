b1396179e29984b1445ec1e0aa16dd14bb97a11c
Steven Gao <stevenruidigao@gmail.com>
2019-01-23 17:53:20 -0500

    Updated djsbot.js

--- djsbot.js
+++ djsbot.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+const YouTube = require('simple-youtube-api');
 const Discord = require('discord.js');
-const musicQueue = new Map();
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 })
 const config = require("./config.json");
 const client = new Discord.Client();
+const youtube = new YouTube(config.googleapikey);
+const musicQueue = new Map();
 
 client.on("ready", () => {
 	console.log("Running!");
@@ -15,30 +17,39 @@ client.on("ready", () => {
     client.user.setActivity("Youtube", { type: "WATCHING"})
 });
 
-client.on("guildCreate", guild => {
-	console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-	client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+client.on('warn', console.warn);
 
-client.on("guildDelete", guild => {
-	console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-	client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
+client.on('error', console.error);
+
+client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
+
+client.on('reconnecting', () => console.log('I am reconnecting now!'));
+
+// client.on("guildCreate", guild => {
+	// console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+	// client.user.setActivity(`Serving ${client.guilds.size} servers`);
+// });
+
+// client.on("guildDelete", guild => {
+	// console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+	// client.user.setActivity(`Serving ${client.guilds.size} servers`);
+// });
 
 client.on("message", async message => {
+	if(message.author.bot) return;
+	console.log(message.content);
 	serverQueue = musicQueue.get(message.guild.id);
 	if (!serverQueue) {
 		serverQueue = {
 			dispatcher: null,
 			connection: null,
 			songs: [],
-			//streamOptions: {seek: 0, volume: 1, passes: 4},
+			streamOptions: {seek: 0, volume: 1, passes: 4},
 			playing: null
 		}
 		// songs[0] is current song
 		musicQueue.set(message.guild.id, serverQueue);
 	}
-	if(message.author.bot) return;
 	var channel = message.channel;
 	const args = message.content.trim().split(/ +/g);
 	const cmd = args[0].toLowerCase();
@@ -46,27 +57,28 @@ client.on("message", async message => {
 	args.shift();
 	// console.log(args);
 	var msg = message.content.toLowerCase();
-	if (cmd.indexOf("hi") > -1 || cmd.indexOf("hello") > -1 || cmd.indexOf("yo") > -1 || cmd.indexOf("hey") > -1) {
+	if (cmd.indexOf("hi") > -1 || cmd.indexOf("hello") > -1 || cmd.indexOf("hey") > -1) {
 		responses = ["Hi", "Hello", "Hey"];
 		suffixes = ["!", " there.", " there!"];
 		channel.send(choice(responses) + choice(suffixes));
 	}
-	if (msg.indexOf(config.prefix) !== 0) return;
 	// console.log(cmd);
-	switch (cmd) {
-		case "!help":
+	switch (cmd.split(config.prefix)[1]) {
+		case "restart":
+			restart(client);
+		case "help":
 			channel.send("Ask @stevenruidigao");
 			break;
-		case "!ping":
+		case "ping":
 			const m = await channel.send("Ping?");
 			m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`).catch(O_o=>{});
 			break;
-		case "!say":
+		case "say":
 			message.delete().catch(O_o=>{}); 
 			const sayMessage = args.join(" ");
 			channel.send(sayMessage);
 			break;
-		case "!idk":
+		case "idk":
 			console.log("IIIDDDKKK: " + cmd + " : " + cmd === "idk");
 			channel.send(
 				"Eu não sei \n" +
@@ -79,7 +91,7 @@ client.on("message", async message => {
 				"Nescio"
 			) 
 			break;
-		case "!type":
+		case "type":
 			message.delete().catch(O_o=>{});
 			if (message.author.discriminator != "6632") {
 				channel.send("You don't have the permission to use this command");
@@ -90,89 +102,172 @@ client.on("message", async message => {
 				channel.send(answer);
 			});
 			break;
-		case "!spam":
+		case "spam":
 			message.delete().catch(O_o=>{});
 			if (args.length > 0) {
 				for (var i = 0; i < args[0]; i ++) channel.send("...");
 			}
 			break;
-		case "!play":
-			id = args[0].split("=")[1];
-			play(message.guild, message.channel, message.member.voiceChannel, id);
+		case "play":
+			message.delete().catch(O_o => {});
+			if (message.member.voiceChannel) play(message.guild, message.channel, message.member.voiceChannel, args[0]);
+			else message.reply("You need to join a voice channel first!");
 			break;	
-		case "!cache":
+		case "cache":
+			// message.channel.send("Deprecated :(");
 			id = args[0].split("=")[1];
 			filename = "cached_music/" + id + ".mp3";
 			ytdl(args[0]).pipe(fs.createWriteStream(filename));
 			console.log("Done!");
 			break;
-		case "!skip":
+		case "skip":
 			serverQueue.playing = null;
 			serverQueue.connection.dispatcher.end();
 			break;
-		case "!stop":
-			serverQueue.songs.length = 0;
-			serverQueue.playing = null;
-			serverQueue.connection.dispatcher.end();
+		case "stop":
+			if (serverQueue.playing != null) {
+				serverQueue.songs.length = 0;
+				serverQueue.playing = null;
+				serverQueue.connection.dispatcher.end();
+			}
 			break;
-		case "!np":
-			message.channel.send("Now Playing: https://www.youtube.com/watch?v=" + serverQueue.songs[0]);
+		case "pause":
+			serverQueue.connection.dispatcher.pause();
 			break;
-		case "!queued":
+		case "resume":
+			serverQueue.connection.dispatcher.resume();
+			break;
+		case "nowplaying":
+			message.channel.send("Now Playing: " + serverQueue.songs[0]);
+			break;
+		case "queued":
 			message.channel.send("Queue: " + serverQueue.songs);
 			break;
 	}
 });
+
 function choice(choices) {
   var index = Math.floor(Math.random() * choices.length);
   return choices[index];
 }
-async function play(guild, channel, voiceChannel, songid) {
-	console.log("sid: " + songid);
-	if (songid == null) {
-		console.log("null");
-		return;
-	}
+
+async function play(guild, channel, voiceChannel, input) {
 	serverQueue = musicQueue.get(guild.id);
-	// console.log(serverQueue.songs);
-	serverQueue.songs.push(songid);
-	console.log("songs1: " + serverQueue.songs);
-	console.log(!(!serverQueue.playing));
-	if (serverQueue.playing) {
+	var url = input;
+	isYTUrl = url.indexOf("=") > -1;
+	if (!isYTUrl) {
+		url = await getYTUrl(input).catch(console.log) + "";
+	}
+	serverQueue.songs.push(url);
+	if (serverQueue.playing != null) {
 		return;
 	}
-	console.log(guild && voiceChannel);
-	if (guild && voiceChannel && !guild.voiceConnection) {
-		await voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+	if (!serverQueue.songs[0]) {
+		voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+	songid = url.split("=")[1];
+	filename = "./cached_music/" + songid + ".mp3";
+	console.log(!fs.existsSync(filename));
+	if (!fs.existsSync(filename)) {
+		console.log("Not using cache :(");
+		await ytdl(url).pipe(fs.createWriteStream(filename));
+	}
+	serverQueue.playing = url;
+	if (guild && voiceChannel) {
+		await voiceChannel.join().then(async connection => { // Connection is an instance of VoiceConnection
 			serverQueue.connection = connection;
-			console.log("Joined channel");
+			channel.send("I have successfully connected to the channel!");
+			const dispatcher = connection.playFile(filename);
+			channel.send("Now Playing: " + url);
+			serverQueue.playing = url;
+			serverQueue.dispatcher = dispatcher;
+			dispatcher.on("end", reason => {
+				console.log(reason);
+				console.log("*" + serverQueue.songs);
+				serverQueue.songs.shift();
+				console.log(serverQueue + "****** shifted " + serverQueue.songs);
+				if (serverQueue.songs.length > 1) {
+					play(guild, channel, voiceChannel, serverQueue.songs[0]);
+				}
+				voiceChannel.leave();
+				console.log("Left channel");
+			});
 		}).catch(console.log);
 	}
-	serverQueue.playing = songid;
-	console.log(serverQueue.playing);
-	filename = "cached_music/" + songid + ".mp3";
-	url = "https://www.youtube.com/watch?v=" + songid;
-	channel.send("Now Playing: " + url);
-	console.log(url);
-	if (fs.existsSync(filename)) {
-		console.log("Using cache!");
-	}
-	else {
-		console.log("Not using cache :(");
-		ytdl(url).pipe(fs.createWriteStream(filename));
-	}
-	const dispatcher = serverQueue.connection.playFile(filename);//, serverQueue.streamOptions);
-	serverQueue.dispatcher = dispatcher;
-	dispatcher.on("end", () => {
-		console.log("*" + serverQueue.songs);
-		if (serverQueue.songs.length > 1) {
-			serverQueue.songs.shift();
-			serverQueue.playing = null;
-			console.log(serverQueue + "****** shifted " + serverQueue.songs);
-			play(guild, channel, voiceChannel, serverQueue.songs[0]);
-		}
-		voiceChannel.leave();
-		console.log("Left channel");
+}
+
+async function getYTUrl(search) {
+	var url;
+	await youtube.searchVideos(search, 1).then((results) => {
+		url = "https://www.youtube.com/watch?v=" + results[0].id;
+	});
+	return url;
+}
+function restart(client) {
+	console.log("Restarting...");
+	client.destroy().then(() => {
+		client.login(config.token);
 	});
 }
 client.login(config.token);
+
+	// console.log("sid: " + songid);
+	// if (songid == null) {
+		// console.log("null");
+		// return;
+	// }
+	// serverQueue = musicQueue.get(guild.id);
+	// console.log(serverQueue.songs);
+	// serverQueue.songs.push(songid);
+	// console.log("songs1: " + serverQueue.songs);
+	// console.log(!(!serverQueue.playing));
+	// if (serverQueue.playing) {
+		// return;
+	// }
+	// console.log(guild && voiceChannel);
+	// if (guild && voiceChannel) {
+		// await voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+			// serverQueue.connection = connection;
+			// console.log("Joined channel");
+		// }).catch(console.log);
+	// }
+	// serverQueue.playing = songid;
+	// console.log(serverQueue.playing);
+	// filename = "cached_music/" + songid + ".mp3";
+	// url = "https://www.youtube.com/watch?v=" + songid;
+	// channel.send("Now Playing: " + url);
+	// console.log(url);
+	// if (fs.existsSync(filename)) {
+		// console.log("Using cache!");
+	// }
+	// else {
+		// try {
+			// console.log("Not using cache :(");
+			// ytdl(url).pipe(fs.createWriteStream(filename));
+		// } catch {
+			// console.log("Searching for: " + songid);
+			// await youtube.searchVideos(songid, 1).then((results) => {
+				// console.log(results);
+				// url =  "https://www.youtube.com/watch?v=" + results[0].id;
+				// console.log(url);
+				// filename = results[0].id +".mp3";
+				// ytdl(url).pipe(fs.createWriteStream(filename));
+				// console.log(serverQueue.connection);
+			// });
+		// }
+	// }
+	// const dispatcher = serverQueue.connection.playFile(filename);//, serverQueue.streamOptions);
+	// serverQueue.dispatcher = dispatcher;
+	// dispatcher.on("end", () => {
+		// console.log("*" + serverQueue.songs);
+		// if (serverQueue.songs.length > 1) {
+			// serverQueue.songs.shift();
+			// serverQueue.playing = null;
+			// console.log(serverQueue + "****** shifted " + serverQueue.songs);
+			// play(guild, channel, voiceChannel, serverQueue.songs[0]);
+		// }
+		// voiceChannel.leave();
+		// console.log("Left channel");
+	// });
